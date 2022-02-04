e8a26bc67dd39ec54a91e59c1b0f5c36988daf00
itaki <mritaki@gmail.com>
2022-02-04 12:58:32 -0500

    created config validator in Shop class that checks to make sure a logging level is there

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ from rudi import shop as shop
 with open('config.json') as json_file:
     config = json.load(json_file)
 
-# set logging level
-LOG_LEVEL = config['env'][0]['logging_level']
-# LOG_LEVEL = os.environ.get('LOG_LEVEL', 'WARNING')
-logging.basicConfig(level=LOG_LEVEL)
+# validate config file before trying to set logging level
+if not shop.Shop.validate_config(shop, config):
+    quit()
+else:
+    # # set logging level
+    LOG_LEVEL = config['env'][0]['logging_level']
+    # LOG_LEVEL = os.environ.get('LOG_LEVEL', 'WARNING')
+    logging.basicConfig(level=LOG_LEVEL)
 
-# start the shop!
-shop1 = shop.Shop()
-shop1.loadConfig(config)
+    # start the shop!
+    shop1 = shop.Shop()
+    shop1.loadConfig(config)
 
 
 
