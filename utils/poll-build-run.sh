24bc8d7f424c9a233dcc9be2b05008cbede6abf8
bigandy99 <bigandy@gmail.com>
2022-11-23 13:05:39 -0500

    simplify to only work from repo root

--- utils/poll-build-run.sh
+++ utils/poll-build-run.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 # inspiration : https://gist.github.com/grant-roy/49b2c19fa88dcffc46ab
 
-# Usage: From the repo root, run "sh utils/poll-build-run.sh"
+# Usage: From the repo root, run "sh utils/poll-build-run.sh" (if you run it from within th utils folder it won't work right)
 # Note: In order to continuously check for updates, docker compose runs in detached mode (i.e. runs the container in the background). If you want to debug live on the pi you should stop the polling script and run docker-compose in normal mode (without the -d param)
 
 
-# make sure this script runs properly regardless of qwhere the user is when they call it
-parent_path=$( cd "$(dirname "$(BASH_SOURCE[0])")" ; pwd -P )
-cd "$parent_path"
-
-#move into the app folder
-cd ..
 cd rudi-app
-
 
 echo "Polling for changes..."
 while true
