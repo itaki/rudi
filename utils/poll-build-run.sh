78fe2750118fe752b5a78b9bc852129e7b5ae235
Andy Harrison <bigandy@gmail.com>
2022-11-22 20:46:51 -0500

    simplify to only monitor current branch

--- utils/poll-build-run.sh
+++ utils/poll-build-run.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 # inspiration : https://gist.github.com/grant-roy/49b2c19fa88dcffc46ab
 
-if [ $# -lt 1 ]; then
-  echo 1>&2 "$0: missing branch parameter"
-  exit 2
-elif [ $# -gt 1 ]; then
-  echo 1>&2 "$0: too many parameters"
-  exit 2
-fi
-
-BRANCH=$1
+#if [ $# -lt 1 ]; then
+#  echo 1>&2 "$0: missing branch parameter"
+#  exit 2
+#elif [ $# -gt 1 ]; then
+#  echo 1>&2 "$0: too many parameters"
+#  exit 2
+#fi
+#BRANCH=$1
 
 cd ..
 cd rudi-app
@@ -18,16 +17,16 @@ while true
 do
 
 
-git fetch origin $BRANCH
+git fetch #origin $BRANCH
 LOCAL=$(git rev-parse HEAD)
 REMOTE=$(git rev-parse @{u})
 
 
 if [ $LOCAL != $REMOTE ]; then
 
-    echo Change Detected!
+    echo Change detected!
+    git pull
 
-    git pull origin $BRANCH;
 
 fi
 sleep 5
