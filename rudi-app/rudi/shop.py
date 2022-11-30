4970ce89d8a77d6d28d70a4cd7785ee98d7777b3
itaki <mritaki@gmail.com>
2022-11-30 15:11:29 -0500

    Edited mock pins warning

--- rudi-app/rudi/shop.py
+++ rudi-app/rudi/shop.py
@@ -35,7 +35,7 @@ class Shop():
     
     def set_pin_mode(self):
         if self.config['hardware']['use_mock_pins']:
-            logging.warning("!!! Using mock pins !!!")
+            logging.warning("Using mock pins!")
             Device.pin_factory = MockFactory()
         return True
 
