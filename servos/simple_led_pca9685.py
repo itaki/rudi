2ac8a6f6ab5a78a9e70e7485b65c5ab853bf4397
itaki <mritaki@gmail.com>
2022-12-12 11:02:43 -0500

    initial commit

---
+++ servos/simple_led_pca9685.py
@@ -0,0 +1,12 @@
+import board
+import busio
+import adafruit_pca9685
+i2c = busio.I2C(board.SCL, board.SDA)
+hat = adafruit_pca9685.PCA9685(i2c, address=0x42)
+
+hat.frequency = 60
+
+led_channel = hat.channels[15]
+
+# from 0x0000 to 0xffff 0 to 65535
+led_channel.duty_cycle = 0xffff
