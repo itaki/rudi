3ab658425fc139e29e611b1877e02b7bc50d9190
itaki <mritaki@gmail.com>
2022-12-24 16:28:25 -0500

    fixed led test

--- servos/simple_led_pca9685.py
+++ servos/simple_led_pca9685.py
@@ -4,11 +4,12 @@ import adafruit_pca9685
 i2c = busio.I2C(board.SCL, board.SDA)
 hat = adafruit_pca9685.PCA9685(i2c, address=0x42)
 
-hat.frequency = 1000
+hat.frequency = 60
 
 led_channel = hat.channels[15]
 
 # from 0x0000 to 0xffff 0 to 65535
-while True:
-    for i =
-led_channel.duty_cycle = 0xffff
+
+for i in range(0xffff):
+    led_channel.duty_cycle = i
+    i = i+1
